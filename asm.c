9e09f3bba090d72d10cee7871c19109f6ef2550e
amandartt <amandarietht@gmail.com>
2017-07-09 15:49:37 -0300

    bug fixes lit numbers

--- asm.c
+++ asm.c
@@ -25,75 +25,130 @@ void asmGen(TAC* first, FILE* output){ //PARA DESCOBRIR OS ASM: gcc -S -O0 track
 									tac->res->text); break;
 			case TAC_ARRAY_VALUE: fprintf(output,	"\t.long %s\n",
 									tac->res->text); break;
-			case TAC_ADD: fprintf(output,	"\n\t## TAC_ADD\n"
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\taddl %s(%%rip), %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break;
-			case TAC_SUB: fprintf(output,	"\n\t## TAC_SUB\n"
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\tsubl %s(%%rip), %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break;
-			case TAC_MUL: fprintf(output,	"\n\t## TAC_MUL\n"
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\timull %s(%%rip), %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break;
+			case TAC_ADD:	fprintf(output,	"\n\t## TAC_ADD\n");
+						  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+						  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op1->text); 
+							else
+								fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op1->text); 	
+							if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+						  		fprintf(output,	"\taddl %s(%%rip), %%eax\n",  tac->op2->text); 
+							else
+								fprintf(output,	"\taddl $%s, %%eax\n",  tac->op2->text); 	 								
+							fprintf(output,	"\tmovl %%eax, %s(%%rip)\n",  tac->res->text); 	 									
+							break;
+			case TAC_SUB: fprintf(output,	"\n\t## TAC_SUB\n");
+						  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+						  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op1->text); 
+							else
+								fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op1->text); 	
+							if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+						  		fprintf(output,	"\tsubl %s(%%rip), %%eax\n",  tac->op2->text); 
+							else
+								fprintf(output,	"\tsubl $%s, %%eax\n",  tac->op2->text); 	 								
+							fprintf(output,	"\tmovl %%eax, %s(%%rip)\n",  tac->res->text); 	 									
+							break;
+			case TAC_MUL: fprintf(output,	"\n\t## TAC_MUL\n");
+						  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+						  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op1->text); 
+							else
+								fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op1->text); 	
+							if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+						  		fprintf(output,	"\timull %s(%%rip), %%eax\n",  tac->op2->text); 
+							else
+								fprintf(output,	"\timull $%s, %%eax\n",  tac->op2->text); 	 								
+							fprintf(output,	"\tmovl %%eax, %s(%%rip)\n",  tac->res->text); 	 									
+							break;
+			// TODO: qdo a divisao é por literal fica uma loucura
 			case TAC_DIV: fprintf(output,	"\n\t## TAC_DIV\n"
 								  			"\tmovl %s(%%rip), %%eax\n"
 											"\tcltd\n"
 											"\tidivl %s(%%rip)\n"
 											"\tmovl %%eax, %s(%%rip)\n",
 								  tac->op1->text,tac->op2->text,tac->res->text); break;
-			case TAC_G: fprintf(output,		"\n\t## TAC_G\n"
-											"\tmovl %s(%%rip), %%edx\n"				
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\tcmpl %%eax, %%edx\n"
-											"\tsetg %%al\n"
-											"\tmovzbl %%al, %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break; 
-			case TAC_L: fprintf(output,		"\n\t## TAC_L\n"
-											"\tmovl %s(%%rip), %%edx\n"				
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\tcmpl %%eax, %%edx\n"
-											"\tsetl %%al\n"
-											"\tmovzbl %%al, %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break; 
-			case TAC_GE: fprintf(output,	"\n\t## TAC_GE\n"
-											"\tmovl %s(%%rip), %%edx\n"				
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\tcmpl %%eax, %%edx\n"
-											"\tsetge %%al\n"
-											"\tmovzbl %%al, %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break; 
-			case TAC_LE: fprintf(output,	"\n\t## TAC_LE\n"
-											"\tmovl %s(%%rip), %%edx\n"				
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\tcmpl %%eax, %%edx\n"
-											"\tsetle %%al\n"
-											"\tmovzbl %%al, %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break; 
-			case TAC_EQ: fprintf(output,	"\n\t## TAC_EQ\n"
-											"\tmovl %s(%%rip), %%edx\n"				
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\tcmpl %%eax, %%edx\n"
-											"\tsete %%al\n"
-											"\tmovzbl %%al, %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break;
-			case TAC_NE: fprintf(output,	"\n\t## TAC_EQ\n"
-											"\tmovl %s(%%rip), %%edx\n"				
-								  			"\tmovl %s(%%rip), %%eax\n"
-											"\tcmpl %%eax, %%edx\n"
-											"\tsetne %%al\n"
-											"\tmovzbl %%al, %%eax\n"
-											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text,tac->op2->text,tac->res->text); break; 
+			case TAC_G: fprintf(output,	"\n\t## TAC_G\n");
+					  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%edx\n",  tac->op1->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%edx\n",  tac->op1->text); 	
+						if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op2->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op2->text); 		
+						fprintf(output,"\tcmpl %%eax, %%edx\n"
+										"\tsetg %%al\n"
+										"\tmovzbl %%al, %%eax\n"
+										"\tmovl %%eax, %s(%%rip)\n",
+							tac->res->text); break; 
+			case TAC_L: fprintf(output,	"\n\t## TAC_L\n");
+					  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%edx\n",  tac->op1->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%edx\n",  tac->op1->text); 	
+						if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op2->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op2->text); 		
+						fprintf(output,"\tcmpl %%eax, %%edx\n"
+										"\tsetl %%al\n"
+										"\tmovzbl %%al, %%eax\n"
+										"\tmovl %%eax, %s(%%rip)\n",
+							tac->res->text); break; 
+			case TAC_GE: fprintf(output,	"\n\t## TAC_GE\n");
+					  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%edx\n",  tac->op1->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%edx\n",  tac->op1->text); 	
+						if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op2->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op2->text); 		
+						fprintf(output,"\tcmpl %%eax, %%edx\n"
+										"\tsetge %%al\n"
+										"\tmovzbl %%al, %%eax\n"
+										"\tmovl %%eax, %s(%%rip)\n",
+							tac->res->text); break; 
+			case TAC_LE:fprintf(output,	"\n\t## TAC_LE\n");
+					  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%edx\n",  tac->op1->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%edx\n",  tac->op1->text); 	
+						if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op2->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op2->text); 		
+						fprintf(output,"\tcmpl %%eax, %%edx\n"
+										"\tsetle %%al\n"
+										"\tmovzbl %%al, %%eax\n"
+										"\tmovl %%eax, %s(%%rip)\n",
+							tac->res->text); break; 
+			case TAC_EQ: fprintf(output,	"\n\t## TAC_EQ\n");
+					  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%edx\n",  tac->op1->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%edx\n",  tac->op1->text); 	
+						if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op2->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op2->text); 		
+						fprintf(output,"\tcmpl %%eax, %%edx\n"
+										"\tsete %%al\n"
+										"\tmovzbl %%al, %%eax\n"
+										"\tmovl %%eax, %s(%%rip)\n",
+							tac->res->text); break; 
+			case TAC_NE: fprintf(output,	"\n\t## TAC_NE\n");
+					  	if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%edx\n",  tac->op1->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%edx\n",  tac->op1->text); 	
+						if(tac->op2->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP)
+					  		fprintf(output,	"\tmovl %s(%%rip), %%eax\n",  tac->op2->text); 
+						else
+							fprintf(output,	"\tmovl $%s, %%eax\n",  tac->op2->text); 		
+						fprintf(output,"\tcmpl %%eax, %%edx\n"
+										"\tsetne %%al\n"
+										"\tmovzbl %%al, %%eax\n"
+										"\tmovl %%eax, %s(%%rip)\n",
+							tac->res->text); break; 
 			case TAC_BEGIN_FUNC: fprintf(output,	"\n\t## TAC_BEGIN_FUNC\n"
 											"\t.text\n"
 											"\t.globl %s\n"
@@ -108,10 +163,17 @@ void asmGen(TAC* first, FILE* output){ //PARA DESCOBRIR OS ASM: gcc -S -O0 track
 			case TAC_RETURN: fprintf(output,"\n\t## TAC_RETURN\n"
 											"\tmovl %s(%%rip), %%eax\n",
 								  tac->res->text); break; 
-			case TAC_MOVE: fprintf(output,"\n\t## TAC_MOVE\n"
+			case TAC_MOVE: if(tac->op1->type == SYMBOL_VAR || tac->op1->type == SYMBOL_VAR_TEMP){ 
+								fprintf(output,"\n\t## TAC_MOVE\n"
 											"\tmovl %s(%%rip), %%eax\n"
 											"\tmovl %%eax, %s(%%rip)\n",
-								  tac->op1->text, tac->res->text); break; 
+								  tac->op1->text, tac->res->text);
+							}else{
+								fprintf(output,"\n\t## TAC_MOVE\n"
+											"\tmovl $%s, %%eax\n"
+											"\tmovl %%eax, %s(%%rip)\n",
+								  tac->op1->text, tac->res->text);
+							} break; 
 			case TAC_IFZ: fprintf(output,	"\n\t## TAC_IFZ\n"
 											"\tmovl %s(%%rip), %%eax\n"
 											"\ttestl %%eax, %%eax\n"
--- asmFile.s
+++ asmFile.s
@@ -1,8 +1,6 @@
+
+.data
 tttemporary_7:
-	.long 0
-tttemporary_26:
-	.long 0
-tttemporary_18:
 	.long 0
 tttemporary_9:
 	.long 0
@@ -12,37 +10,23 @@ tttemporary_0:
 	.long 0
 tttemporary_10:
 	.long 0
-tttemporary_23:
-	.long 0
 tttemporary_2:
 	.long 0
 tttemporary_17:
 	.long 0
-tttemporary_27:
-	.long 0
 tttemporary_4:
-	.long 0
-tttemporary_20:
 	.long 0
 tttemporary_13:
 	.long 0
 tttemporary_6:
 	.long 0
-tttemporary_24:
-	.long 0
 tttemporary_8:
 	.long 0
 tttemporary_16:
 	.long 0
-tttemporary_21:
-	.long 0
 tttemporary_12:
 	.long 0
 tttemporary_1:
-	.long 0
-tttemporary_25:
-	.long 0
-tttemporary_19:
 	.long 0
 tttemporary_3:
 	.long 0
@@ -52,205 +36,254 @@ tttemporary_5:
 	.long 0
 tttemporary_11:
 	.long 0
-tttemporary_22:
-	.long 0
+.LC0:
+	.string "%d"
+.LC1:
+	.string "\n"
+.LC2:
+	.string "%d"
+.LC3:
+	.string "\n"
+.LC4:
+	.string "%d"
+.LC5:
+	.string "\n"
+.LC6:
+	.string "%d"
+.LC7:
+	.string "\n"
+.LC8:
+	.string "dentro while\n"
+.LC9:
+	.string "fora while"
+.LC10:
+	.string "a:"
+.LC11:
+	.string "%d"
+.LC12:
+	.string "\n"
+.LC13:
+	.string "ok1 a > 11"
+.LC14:
+	.string "ok2 a <= 11"
 a:
-	.long 0
+	.long 2
 i:
 	.long 0
-v:
-	.long 1
-	.long 3
 
 	## TAC_BEGIN_FUNC
-
-.globl main
+	.text
+	.globl main
 main:
 	.cfi_startproc
 	pushq	%rbp
 
-	## TAC_VEC_READ
-	movl v+8(%rip), %eax
-	movl %eax, tttemporary_14(%rip)
-
-	## TAC_VEC_WRITE
-	movl tttemporary_14(%rip), %eax
-	movl %eax, v+4(%rip)
-
-	## TAC_ADD
-	movl 1(%rip), %eax
-	addl 1(%rip), %eax
-	movl %eax, tttemporary_15(%rip)
-
-	## TAC_VEC_READ
-	movl tttemporary_15(%rip), %eax
-	cltq
-	movl v(,%rax, 4), %eax
-	movl %eax, tttemporary_16(%rip)
-
-	## TAC_VEC_WRITE
-	movl tttemporary_16(%rip), %eax
-	movl %eax, v+4(%rip)
-
-	## TAC_ADD
-	movl 10(%rip), %eax
-	addl a(%rip), %eax
-	movl %eax, tttemporary_17(%rip)
+	## TAC_SUB
+	movl a(%rip), %eax
+	subl $1, %eax
+	movl %eax, tttemporary_9(%rip)
 
 	## TAC_MOVE
-	movl tttemporary_17(%rip), %eax
+	movl tttemporary_9(%rip), %eax
 	movl %eax, a(%rip)
 
-	## TAC_SUB
-	movl i(%rip), %eax
-	subl a(%rip), %eax
-	movl %eax, tttemporary_18(%rip)
+	## TAC_PRINT: VAR
+	movl a(%rip), %eax
+	movl %eax, %esi
+	movl $.LC0, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_PRINT: STRING
+	movl $.LC1, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_ADD
+	movl a(%rip), %eax
+	addl $1, %eax
+	movl %eax, tttemporary_10(%rip)
 
 	## TAC_MOVE
-	movl tttemporary_18(%rip), %eax
-	movl %eax, i(%rip)
+	movl tttemporary_10(%rip), %eax
+	movl %eax, a(%rip)
+
+	## TAC_PRINT: VAR
+	movl a(%rip), %eax
+	movl %eax, %esi
+	movl $.LC2, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_PRINT: STRING
+	movl $.LC3, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_MUL
+	movl a(%rip), %eax
+	imull $2, %eax
+	movl %eax, tttemporary_11(%rip)
+
+	## TAC_MOVE
+	movl tttemporary_11(%rip), %eax
+	movl %eax, a(%rip)
+
+	## TAC_PRINT: VAR
+	movl a(%rip), %eax
+	movl %eax, %esi
+	movl $.LC4, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_PRINT: STRING
+	movl $.LC5, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_DIV
+	movl a(%rip), %eax
+	cltd
+	idivl 2(%rip)
+	movl %eax, tttemporary_12(%rip)
+
+	## TAC_MOVE
+	movl tttemporary_12(%rip), %eax
+	movl %eax, a(%rip)
+
+	## TAC_PRINT: VAR
+	movl a(%rip), %eax
+	movl %eax, %esi
+	movl $.LC6, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_PRINT: STRING
+	movl $.LC7, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_MOVE
+	movl $0, %eax
+	movl %eax, a(%rip)
+
+	## TAC_LABEL
+.ttlabel_4:
+
+	## TAC_L
+	movl a(%rip), %edx
+	movl $2, %eax
+	cmpl %eax, %edx
+	setl %al
+	movzbl %al, %eax
+	movl %eax, tttemporary_13(%rip)
+
+	## TAC_L
+	movl a(%rip), %edx
+	movl $5, %eax
+	cmpl %eax, %edx
+	setl %al
+	movzbl %al, %eax
+	movl %eax, tttemporary_14(%rip)
+
+	## TAC_OR
+	movl tttemporary_13(%rip), %eax
+	testl %eax, %eax
+	je .L0
+	movl $1, tttemporary_15(%rip)
+	jmp .L2
+.L0:
+	movl tttemporary_14(%rip), %eax
+	testl %eax, %eax
+	je .L1
+	movl $1, tttemporary_15(%rip)
+	jmp .L2
+.L1:
+	movl $0, tttemporary_15(%rip)
+.L2:
+
+	## TAC_IFZ
+	movl tttemporary_15(%rip), %eax
+	testl %eax, %eax
+	je .ttlabel_5
+
+	## TAC_PRINT: STRING
+	movl $.LC8, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_ADD
+	movl a(%rip), %eax
+	addl $1, %eax
+	movl %eax, tttemporary_16(%rip)
+
+	## TAC_MOVE
+	movl tttemporary_16(%rip), %eax
+	movl %eax, a(%rip)
+
+	## TAC_JUMP
+	jmp .ttlabel_4
+
+	## TAC_LABEL
+.ttlabel_5:
+
+	## TAC_PRINT: STRING
+	movl $.LC9, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_PRINT: STRING
+	movl $.LC10, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_PRINT: VAR
+	movl a(%rip), %eax
+	movl %eax, %esi
+	movl $.LC11, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_PRINT: STRING
+	movl $.LC12, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_MOVE
+	movl $250, %eax
+	movl %eax, a(%rip)
+
+	## TAC_G
+	movl a(%rip), %edx
+	movl $11, %eax
+	cmpl %eax, %edx
+	setg %al
+	movzbl %al, %eax
+	movl %eax, tttemporary_17(%rip)
+
+	## TAC_IFZ
+	movl tttemporary_17(%rip), %eax
+	testl %eax, %eax
+	je .ttlabel_6
+
+	## TAC_PRINT: STRING
+	movl $.LC13, %edi
+	movl $0, %eax
+	call printf
+
+	## TAC_JUMP
+	jmp .ttlabel_7
 
 	## TAC_LABEL
 .ttlabel_6:
 
-	## TAC_G
-	movl a(%rip), %edx
-	movl 0(%rip), %eax
-	cmpl %eax, %edx
-	setg %al
-	movzbl %al, %eax
-	movl %eax, tttemporary_19(%rip)
-
-	## TAC_G
-	movl i(%rip), %edx
-	movl 0(%rip), %eax
-	cmpl %eax, %edx
-	setg %al
-	movzbl %al, %eax
-	movl %eax, tttemporary_20(%rip)
-
-	## TAC_OR
-	movl tttemporary_19(%rip), %eax
-	testl %eax, %eax
-	je .L0
-	movl $1, tttemporary_21(%rip)
-	jmp .L2
-.L0:
-	movl tttemporary_20(%rip), %eax
-	testl %eax, %eax
-	je .L1
-	movl $1, tttemporary_21(%rip)
-	jmp .L2
-.L1:
-	movl $0, tttemporary_21(%rip)
-.L2:
-
-	## TAC_IFZ
-	movl tttemporary_21(%rip), %eax
-	testl %eax, %eax
-	je .ttlabel_7
-
-	## TAC_ADD
-	movl a(%rip), %eax
-	addl 1(%rip), %eax
-	movl %eax, tttemporary_22(%rip)
-
-	## TAC_MOVE
-	movl tttemporary_22(%rip), %eax
-	movl %eax, a(%rip)
-
-	## TAC_JUMP
-	jmp .ttlabel_6
+	## TAC_PRINT: STRING
+	movl $.LC14, %edi
+	movl $0, %eax
+	call printf
 
 	## TAC_LABEL
 .ttlabel_7:
-
-	## TAC_LABEL
-.ttlabel_8:
-
-	## TAC_G
-	movl a(%rip), %edx
-	movl 0(%rip), %eax
-	cmpl %eax, %edx
-	setg %al
-	movzbl %al, %eax
-	movl %eax, tttemporary_23(%rip)
-
-	## TAC_G
-	movl i(%rip), %edx
-	movl 0(%rip), %eax
-	cmpl %eax, %edx
-	setg %al
-	movzbl %al, %eax
-	movl %eax, tttemporary_24(%rip)
-
-	## TAC_AND
-	movl tttemporary_23(%rip), %eax
-	testl %eax, %eax
-	jne .L3
-	movl $0, tttemporary_25(%rip)
-	jmp .L5
-.L3:
-	movl tttemporary_24(%rip), %eax
-	testl %eax, %eax
-	je .L4
-	movl $1, tttemporary_25(%rip)
-	jmp .L5
-.L4:
-	movl $0, tttemporary_25(%rip)
-.L5:
-
-	## TAC_IFZ
-	movl tttemporary_25(%rip), %eax
-	testl %eax, %eax
-	je .ttlabel_9
-
-	## TAC_ADD
-	movl a(%rip), %eax
-	addl 1(%rip), %eax
-	movl %eax, tttemporary_26(%rip)
-
-	## TAC_MOVE
-	movl tttemporary_26(%rip), %eax
-	movl %eax, a(%rip)
-
-	## TAC_JUMP
-	jmp .ttlabel_8
-
-	## TAC_LABEL
-.ttlabel_9:
-
-	## TAC_MOVE
-	movl a(%rip), %eax
-	movl %eax, i(%rip)
-
-	## TAC_LABEL
-.ttlabel_10:
-
-	## TAC_IFLESSEQ
-	movl i(%rip), %eax
-	cmpl 10(%rip), %eax
-	jg .ttlabel_11
-
-	## TAC_MUL
-	movl a(%rip), %eax
-	imull 50(%rip), %eax
-	movl %eax, tttemporary_27(%rip)
-
-	## TAC_MOVE
-	movl tttemporary_27(%rip), %eax
-	movl %eax, i(%rip)
-
-	## TAC_INC
-	movl i(%rip), %eax
-	addl $1, %eax
-	movl %eax, i(%rip)
-
-	## TAC_JUMP
-	jmp .ttlabel_10
-
-	## TAC_LABEL
-.ttlabel_11:
 
 	## TAC_RETURN
 	movl a(%rip), %eax
